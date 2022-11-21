120042c7386d48a373e946c2276a2d66097d10dd
Ludovic Lahougue <ludo.lahougue@orange.fr>
2022-11-21 15:24:46 +0100

    .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,18 @@ import { UsersModule } from './users/users.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'user',
+      type: "mysql",
+      host: process.env.SQL_HOST,
+      port: parseInt(process.env.SQL_PORT ?? ''),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       synchronize: true,
       autoLoadEntities: true,
     }),
